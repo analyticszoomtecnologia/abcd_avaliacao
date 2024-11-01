ff800a6d92263daed3078305de36286930f6149e
Gabriel Cordeiro <cordeirodados@gmail.com>
2024-11-01 18:30:51 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,8 @@ else:
         user_id = st.session_state["id_emp"]
         link_abcd = f"{link_abcd_base}?user_id={urllib.parse.quote(str(user_id))}"  # Passa apenas o user_id
 
-        st.write("Redirecionando para a página principal...")
-        st.markdown(f"[Clique aqui se não for redirecionado automaticamente.]({link_abcd})", unsafe_allow_html=True)
+        #st.write("Redirecionando para a página principal...")
+        st.markdown(f"[Clique aqui para Realizar Avaliação.]({link_abcd})", unsafe_allow_html=True)
 
     elif pagina_selecionada == "Funcionários Data":
         func_data_page()
