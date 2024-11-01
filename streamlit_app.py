99124d2acb7373f68bba145235b0c45db19a9d95
Gabriel Cordeiro <cordeirodados@gmail.com>
2024-11-01 18:30:18 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,3 +34,17 @@ else:
         func_data_page()
     elif pagina_selecionada == "Lista de Avaliados":
         func_data_nota()
+
+st.markdown(
+        """
+        <br><hr>
+        <div style='text-align: center;'>
+            Desenvolvido por 
+            <a href='https://www.linkedin.com/in/gabriel-cordeiro-033641144/' target='_blank' style='text-decoration: none; color: #0A66C2;'>
+                <img src='https://upload.wikimedia.org/wikipedia/commons/f/f8/LinkedIn_icon_circle.svg' alt='LinkedIn' width='20' style='vertical-align: middle; margin-right: 5px;' />
+                Gabriel Cordeiro
+            </a>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
