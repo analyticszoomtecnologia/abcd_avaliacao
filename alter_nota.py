026e03476e5a047d5fee435d153b722aeb025be3
analyticszoomtecnologia <analyticsdatazoom@gmail.com>
2024-12-30 13:46:51 -0300

    Update alter_nota.py

--- alter_nota.py
+++ alter_nota.py
@@ -149,19 +149,19 @@ def func_data_nota():
     else:
         st.error("Não foi possível conectar ao banco de dados.")
     
-    st.markdown(
-        """
-        <br><hr>
-        <div style='text-align: center;'>
-            Desenvolvido por 
-            <a href='https://www.linkedin.com/in/gabriel-cordeiro-033641144/' target='_blank' style='text-decoration: none; color: #0A66C2;'>
-                Gabriel Cordeiro
-                <img src='https://upload.wikimedia.org/wikipedia/commons/f/f8/LinkedIn_icon_circle.svg' alt='LinkedIn' width='20' style='vertical-align: middle; margin-right: 5px;' />
-            </a>
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
+    #st.markdown(
+        #"""
+        #<br><hr>
+        #<div style='text-align: center;'>
+            #Desenvolvido por 
+            #<a href='https://www.linkedin.com/in/gabriel-cordeiro-033641144/' target='_blank' style='text-decoration: none; color: #0A66C2;'>
+                #Gabriel Cordeiro
+                #<img src='https://upload.wikimedia.org/wikipedia/commons/f/f8/LinkedIn_icon_circle.svg' alt='LinkedIn' width='20' style='vertical-align: middle; margin-right: 5px;' />
+            #</a>
+        #</div>
+        #""",
+        #unsafe_allow_html=True
+    #)
 
 
 def buscar_por_nome(conn, nome, subordinados):
@@ -191,16 +191,16 @@ def deletar_avaliado(conn, id_emp):
     except Exception as e:
         st.error(f"Erro ao deletar: {e}")
 
-    st.markdown(
-    """
-    <br><hr>
-    <div style='text-align: center;'>
-        Desenvolvido por 
-        <a href='https://www.linkedin.com/in/gabriel-cordeiro-033641144/' target='_blank' style='text-decoration: none; color: #0A66C2;'>
-            <img src='https://upload.wikimedia.org/wikipedia/commons/f/f8/LinkedIn_icon_circle.svg' alt='LinkedIn' width='20' style='vertical-align: middle; margin-right: 5px;' />
-            Gabriel Cordeiro
-        </a>
-    </div>
-    """,
-    unsafe_allow_html=True
-)
+    #st.markdown(
+    #"""
+    #<br><hr>
+    #<div style='text-align: center;'>
+        #Desenvolvido por 
+        #<a href='https://www.linkedin.com/in/gabriel-cordeiro-033641144/' target='_blank' style='text-decoration: none; color: #0A66C2;'>
+            #<img src='https://upload.wikimedia.org/wikipedia/commons/f/f8/LinkedIn_icon_circle.svg' alt='LinkedIn' width='20' style='vertical-align: middle; margin-right: 5px;' />
+            #Gabriel Cordeiro
+        #</a>
+    #</div>
+    #""",
+    #unsafe_allow_html=True
+#)
